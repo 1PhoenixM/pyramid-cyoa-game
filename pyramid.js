69b431cb1afaa0abe16a392474d3459e14128da7
Melissa I <phoenixemere@gmail.com>
2013-12-03 23:45:03 -0500

    Final version - no images.

--- pyramid.js
+++ pyramid.js
@@ -1,4 +1,5 @@
 //V1.2 BETA
+//By Melissa Iori & Mackenzie Hummel
 ////////////////////////
 //Game Mechanics:
 //Player exists as an object with a customizable name, and choice of three personalities which slightly affect the storyline.
@@ -8,29 +9,12 @@
 //Enemy battles have fixed positions, but fights are based on RNG (random number generator).
 //Help windows are available at all times to explain controls.
 //Invalid name/personality input values are handled.
-////////////////////////
-//Left to do: minor bugfixes, images, CSS, comments.
-//Bugs Fixed:
-//progresscontrol buttons are no longer removed if player declines battle
-//added by M.I.: an event now occurs if you use the flashlight in the very first room. needs an image.
-//second boss now blocks you without item.
-//items can only be picked up once now.
-//enemies now (apparently) at different chance values through a separate check function. not sure if working.
 ///////////////////////
-//Known bugs: 
-//directional buttons appear in boss fights, allowing player to bypass the boss fight and move on
-//could add different messages other than "missed" in enemy fights
-//add possible deaths after 10+ missed attacks
-//if directional buttons are used when asked if you want to fight/pick up something, the fight/item buttons are left intact in other rooms.
-///////////////////////
-//enemies respawn (should they permanently die?)
-//		It kind of goes along with the whole pyramid theme that they keep resurrecting... plus it lengthens the game?
-//enemy randomness may be too high (I had an irritating run when I was playing where I didn't hit the enemy after 15 or so attempts
-//		Sometimes they die on the first try, too...
-///////////////////////
+
 
 walled="There's a wall or other impass blocking you. Try another way.";
 playerinventory=[];
+
 
 function changeRoom(newRoom){ //changes room according to player movement
 	var x; //room description
@@ -44,109 +28,159 @@ function changeRoom(newRoom){ //changes room according to player movement
 			else{x="It is dark in here.";}
 			break;
 		case 'swordboss':
-			x="A large enemy appeared! A sword would be helpful here! Will you fight?</br>";
+			x="A figure in bandages appeared! A sword might be helpful here! Will you fight?</br>";
 			itemcheck();
-			enemyfight(6);
 			document.getElementById('fightConfirm').style.display ='block';
+			document.getElementById('itemConfirm').style.display ='none';
+			if (bossflag1===false){
+			document.getElementById('progresscontrol').style.display ='none';
+			}
+			else {
+			document.getElementById('progresscontrol').style.display ='block';
+			}
 			break;
 		case 'axeboss':
-			x="A second large enemy appeared! A pickaxe would be helpful here! Will you fight?</br>";
+			x="An enormous cobra appeared! A pick-axe might be helpful here! Will you fight?</br>";
 			itemcheck();
-			enemyfight(10);
 			document.getElementById('fightConfirm').style.display ='block';
+			document.getElementById('itemConfirm').style.display ='none';
+			if (bossflag2===false){
+			document.getElementById('progresscontrol').style.display ='none';
+			}
+			else {
+			document.getElementById('progresscontrol').style.display ='block';
+			}
 			break;
 		case 'enemy1':
-			x="The first small enemy appeared! Will you fight?</br>";
-			enemyfight(3);
+			x="A tiny tarantula appeared! Will you fight?</br>";
 			document.getElementById('fightConfirm').style.display ='block';
+			document.getElementById('itemConfirm').style.display ='none';
 			break;
 		case 'enemy2':
-			x="A second small enemy appears. Will you fight?</br>";
-			enemyfight(6);
+			x="A small cobra appeared. Will you fight?</br>";
 			document.getElementById('fightConfirm').style.display ='block';
+			document.getElementById('itemConfirm').style.display ='none';
 			break;
 		case 'enemy3':
-			x="A third small enemy appears. Will you fight?</br>";
-			enemyfight(7);
+			x="An angry rat appears. Will you fight?</br>";
 			document.getElementById('fightConfirm').style.display ='block';
+			document.getElementById('itemConfirm').style.display ='none';
 			break;
 		case 'swordroom':
+			document.getElementById('fightConfirm').style.display ='none';
 			if(playerinventory[0]==='sword'){
 			x="It appears to be an old armory... you already took the sword, so there's not much to see.";
 			}
 			else{x="There is an ancient-looking sword mounted on the far wall. Will you take it?";
-			document.getElementById('itemConfirm').style.display ='block';}
+			document.getElementById('itemConfirm').style.display ='block';
+			}
 			break;
 		case 'axeroom':
+			document.getElementById('fightConfirm').style.display ='none';
 			if(playerinventory[1]==='pick-axe'){
 			x="There were people here more recently than you would have guessed... you already took the pick-axe, so there's not much to see.";
 			}
 			else{x="A pickaxe leans in the corner. Will you take it?";
-			document.getElementById('itemConfirm').style.display ='block';}
+			document.getElementById('itemConfirm').style.display ='block';
+			}
 			break;
 		case 'maskroom':
 			x="A Strange Mask lies on the floor. Will you take it?";
 			document.getElementById('itemConfirm').style.display ='block';
+			document.getElementById('fightConfirm').style.display ='none';
 			break;
 		case 'lightroom':
+			document.getElementById('fightConfirm').style.display ='none';
 			if(playerinventory[3]==='flashlight'){
 			x="There were definitely people in this area very recently. Maybe the flashlight could help you illuminate some dark secrets of your own.";
 			}
 			else{x="There is a flashlight on the floor. Will you take it?";
-			document.getElementById('itemConfirm').style.display ='block';}
+			document.getElementById('itemConfirm').style.display ='block';
+			}
 			break;
 		case 'trove':
 			x="You find a trove of treasure in this room. But it\'s probably cursed, so you leave it alone.";
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
 			break;
 		case 'danger':
 			x="You feel an ominous sense of danger.";
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
+			document.getElementById('progresscontrol').style.display='block';
 			break;
 		case 'almost':
 			x="There is a light ahead of you, but maybe you should check around a bit before you go.";
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
 			break;
 		case 'dogs':
 			x="You find a room full of what appears to be mummified dogs. You feel safe somehow.";
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
 			break;
 		case 'goldtunnel':
 			x="You enter a narrow tunnel lined with gold.";
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
 			break;
 		case 'finish':
 			x="You made it out okay! Thank you for playing!";
+			death(4);
 			break;
 		case 'abyss':
 			x="You find a deep, dark staircase spiraling down into inky blackness. You can see that the stairs end, crumbling off into the abyss. It\'s impossible to go any further.";
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
 			break;
 		case 'vista':
 			x="You see the outside for the first time. It\'s a beautiful vista from where you are, but you can\'t go any further.";
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
 			break;
 		case 'maskreject':
 			x="Passed the Strange Mask up. It disintegrates. Good thing, too - it was cursed.";
 			break;
 		case 'note2':
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
 			if(player.pers==='benevolent'){x="There are interesting engravings on the wall. This one reads: the kindness of " + player.pname + " was known everywhere. ";}
 			else if(player.pers==='brave'){x="There are interesting engravings on the wall. This one reads: the conquests of " + player.pname + " will be known throughout time. ";}
 			else if(player.pers==='powerful'){x="There are interesting engravings on the wall. This one reads: the wrath of " + player.pname + " will not be forgotten. ";}
 			break;
 		case 'note3':
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
 			if(player.pers==='benevolent'){x="There are interesting engravings on the wall. This one reads: here we have left the belongings of " + player.pname + ". We hope what they have given in charity will be paid back in full in the afterlife. ";}
 			else if(player.pers==='brave'){x="There are interesting engravings on the wall. This one reads: the battle trappings of " + player.pname + " lie here with their master for all time. ";}
 			else if(player.pers==='powerful'){x="There are interesting engravings on the wall. This one reads: the vast riches of " + player.pname + " rest here. May the gods help thee who disturb them. ";}
 			break;
 		case 'note4':
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
 			if(player.pers==='benevolent'){x="There are interesting engravings on the wall. This one reads: Here lies " + player.pname + ", our just ruler and savior. ";}
 			else if(player.pers==='brave'){x="There are interesting engravings on the wall. This one reads: Here lies " + player.pname + ", our ruler and conqueror of many, who sacrificed for all. ";}
 			else if(player.pers==='powerful'){x="There are interesting engravings on the wall. This one reads: Here lies " + player.pname + ", a ruler greatly feared, but loved. ";}
 			break;
 		case 'staircase':
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
 			x="You find a dark staircase.";
 			break;
 		case 'smallroom':
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
 			x="You come across a small room.";
 			break;
 		case 'passage':
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
+			document.getElementById('progresscontrol').style.display='block';
 			x="This passage is lit by torches.";
 			break;
 		case 'note1':
+			document.getElementById('fightConfirm').style.display ='none';
+			document.getElementById('itemConfirm').style.display ='none';
 			if(player.pers==='benevolent'){x="There are interesting engravings on the wall. This one reads: glory to the kind and generous " + player.pname + ". ";}
 			else if(player.pers==='brave'){x="There are interesting engravings on the wall. This one reads: remembrance of " + player.pname + "\'s deeds. ";}
 			else if(player.pers==='powerful'){x="There are interesting engravings on the wall. This one reads: the inimitable strength of " + player.pname + ". ";}
@@ -155,30 +189,41 @@ function changeRoom(newRoom){ //changes room according to player movement
 	document.getElementById('outputDiv').innerHTML=x; //displays new room description
 }
 
-function death(x) { //chooses what type of death the player experiences and resets the game
-	if (x===0) {
+function death(how) { //chooses what type of death the player experiences and resets the game
+	if (how===0) {
 		document.getElementById('outputDiv').innerHTML="You have fallen into a pit of thorns! *YOU HAVE DIED*";        
 	}
-	else if (x===1) {
+	else if (how===1) {
 		document.getElementById('outputDiv').innerHTML="The walls begin to close in on you and you can\'t stop them. *YOU HAVE DIED*";
 	}
-	else if (x===2) {
+	else if (how===2) {
 		document.getElementById('outputDiv').innerHTML="The room is filled with deadly snakes. *YOU HAVE DIED*";
 	}
-	else if (x===3) {
+	else if (how===3) {
 		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+"Bad idea - the strange mask was cursed. You meet with an unearthly fate... *YOU HAVE DIED*";
+	}
+	else if (how===4) {
+		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML;
+	}
+	else if (how===5) {
+		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+"The enemy overpowered you... *YOU HAVE DIED*";
 	}
 	document.getElementById('retry').style.display='block';
 	document.getElementById('gameControls').style.display='none';
 }
 
 function master(){ //starts the game
+	bossflag1=false;
+	bossflag2=false;
 	player = {};
 	player['pname']=document.getElementById('nameBox').value;
 	player['pers']=document.getElementById('persBox').value.toLowerCase();
 
 	if (player.pname===''){
-		document.getElementById('outputDiv').innerHTML='Please type your name into the box.';
+		alert('Please type your name into the box.');
+	}
+	if (player.pers==='' || !(player.pers==='brave' || player.pers==='powerful'|| player.pers==='benevolent')){
+		alert('Please choose between "benevolent," "powerful," or "brave" and type the word into the personality box.');
 	}
 	else if(player.pname!==''){
 		document.getElementById('namecontrol').style.display = 'none';
@@ -190,9 +235,7 @@ function master(){ //starts the game
 			document.getElementById('outputDiv').innerHTML="Welcome, " + player.pname + ". It is dark in here." //start room is now context sensitive and will only welcome the player once                
 			document.getElementById('currentRoom').value='start'; //hidden div in html tells direction functions where the player is on the map. value 'start' is the first room.
 		}
-		if (player.pers===''){
-			document.getElementById('outputDiv').innerHTML = '';
-		}
+		
 	}
 }
 
@@ -471,8 +514,14 @@ function backwards() { //user moves back one room to the previous room
 
 
 function itemno(){ //if item is declined
-	document.getElementById('outputDiv').innerHTML="Decided not to take it.";
-	document.getElementById('itemConfirm').style.display = 'none';
+	currentRoom=document.getElementById('currentRoom').value;	
+	if(currentRoom==='start'){
+		document.getElementById('outputDiv').innerHTML="You know you want to.";
+	}
+	else{
+		document.getElementById('outputDiv').innerHTML="Decided not to take it.";
+		document.getElementById('itemConfirm').style.display = 'none';
+	}
 }
 
 function itemhandler(){ //adds items to array if taken
@@ -494,7 +543,7 @@ function itemhandler(){ //adds items to array if taken
 		playerinventory[3]='flashlight';
 	}
 	else if (currentRoom==='start'){
-		document.getElementById('outputDiv').innerHTML="The room has become illuminated. On the wall is a brilliant mural depicting yourself, and your loyal animal companions.";
+		document.getElementById('outputDiv').innerHTML="The room has become illuminated. On the wall is a brilliant mural depicting yourself, and your loyal canine companions.";
 	}
 	else {
 		return;
@@ -511,20 +560,60 @@ function inventory() { //shows inventory to player
 	else if (playerinventory[0]==='sword' && playerinventory[1]==='pick-axe' && playerinventory[3]==='flashlight') {
 		alert(playerinventory[0]+', '+playerinventory[1]+', '+playerinventory[3]);
 	}
+	else if (playerinventory[0]!=='sword'){
+		alert('You have no items.');
+	}
 }
 
 function enemyfight(chance){ //handles enemy encounters
+	currentRoom=document.getElementById('currentRoom').value;
 	var chance, enemyChance=Math.floor((Math.random()*chance)+1);
-	
 	if (enemyChance!==2){
 		enemyChance=Math.floor((Math.random()*chance)+1);
+		if (enemyChance===1){
 		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+'Missed! So close! Attack again?</br>';
+		}
+		else if (enemyChance===3){
+		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+'The enemy sidestepped...Attack again?</br>';
+		}
+		else if (enemyChance===4){
+		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+'The enemy dodged easily. Attack again?</br>';
+		}
+		else if (enemyChance===5){
+		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+'A near miss! Attack again?</br>';
+		}
+		else if (enemyChance===6){
+		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+'The enemy was unaffected! Attack again?</br>';
+		}
+		else if (enemyChance===7){
+		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+'The enemy ignored you...Attack again?</br>';
+		}
+		else if (enemyChance===8){
+		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+'The enemy craftily evaded you. Attack again?</br>';
+		}
+		else if (enemyChance===9){
+		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+'Your attack failed! Attack again?</br>';
+		}
+		else if (enemyChance===10){
+		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+'It barely did a thing...Attack again?</br>';
+		}
+		else {
+		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+'The enemy is laughing at you! Attack again?</br>';
+		}
+		
 	}
 	else{
 		document.getElementById('outputDiv').innerHTML=document.getElementById('outputDiv').innerHTML+'You defeated it!</br>';
 		document.getElementById('progresscontrol').style.display='block';
 		document.getElementById('fightConfirm').style.display='none';
+		if (currentRoom==='swordboss'){
+		bossflag1=true;
+		}
+		else if(currentRoom==='axeboss'){
+		bossflag2=true;
+		}
 	}
+	
 }
 
 
@@ -570,11 +659,11 @@ function enemycheck(){ //decides difficulty of enemies
 	}
 }
 
-function help(x){ //changes help message depending on if game has started or not
-	if (x===0) {        
+function help(msg){ //changes help message depending on if game has started or not
+	if (msg===0) {        
 		alert('Enter your name into the first box prompt, and personality (benevolent, brave, or powerful) into the second box prompt and click "CONFIRM" to start. Be aware that there are no continues, and that refreshing the page will erase your progress.');
 	}
-	else if (x===1) {
+	else if (msg===1) {
 		alert('Choose which direction you\'d like to go. Click "INVENTORY" to view the items you have collected on your journey.')
 	}
 }
